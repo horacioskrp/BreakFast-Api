e76b0b098ec99a4cfd1b2cf8bf288d835c7f4641
horacio_skrp <hervekudayah@gmail.com>
2023-11-11 22:28:06 +0000

    add Dto -> record to Contract sl 💫

--- BreakFast.Contracts/BreakFast/BreakfastResponse.cs
+++ BreakFast.Contracts/BreakFast/BreakfastResponse.cs
@@ -1,10 +1,12 @@
 namespace BreakFast.Contracts.BreakFast;
 
-public record CreateBreakfastRequest(
+public record BreakfastResponse(
+    Guid Id,
     string Name,
     string Description,
     DateTime StartDateTime,
     DateTime EndDateTime,
+    DateTime LastModify,
     List<string> Savory,
     List<string> Sweet
 );
--- BreakFast.Contracts/BreakFast/UpsertBreakfastRequest.cs
+++ BreakFast.Contracts/BreakFast/UpsertBreakfastRequest.cs
@@ -1,10 +1,11 @@
 namespace BreakFast.Contracts.BreakFast;
+public record UpsertBreakfastRequest(
 
-public record CreateBreakfastRequest(
     string Name,
     string Description,
     DateTime StartDateTime,
     DateTime EndDateTime,
+    DateTime LastModify,
     List<string> Savory,
     List<string> Sweet
 );
